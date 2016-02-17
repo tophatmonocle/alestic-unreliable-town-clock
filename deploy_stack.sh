4cf03b9f52cb12ae958eeddd16aa18876bde5c44
Corey Gale <corey@tophat.com>
2016-02-17 15:57:36 -0500

    Adding deploy shell script

---
+++ deploy_stack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+aws cloudformation create-stack \
+  --stack-name "town-clock" \
+  --capabilities CAPABILITY_IAM \
+  --template-body file://./unreliable-town-clock.template \
+  --parameters \
+  ParameterKey='ClockPeriod',ParameterValue='10',UsePreviousValue=false \
+  ParameterKey='InstallScriptURL',ParameterValue='https://raw.githubusercontent.com/tophatmonocle/alestic-unreliable-town-clock/master/unreliable-town-clock-bootstrap',UsePreviousValue=false \
+  ParameterKey='InstanceType',ParameterValue='t2.micro',UsePreviousValue=false \
+  ParameterKey='OperatorEmail',ParameterValue='corey@tophat.com',UsePreviousValue=false \
+  ParameterKey='SshKeyName',ParameterValue='gateway',UsePreviousValue=false \
+  ParameterKey='Support',ParameterValue='',UsePreviousValue=false \
+  ParameterKey='VPCZoneId',ParameterValue='subnet-757b6f5d',UsePreviousValue=false \
+  ParameterKey='AvailabilityZone',ParameterValue='us-east-1e',UsePreviousValue=false

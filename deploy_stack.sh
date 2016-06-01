8651e89ea9f3cde69f014714713d974918c46d5c
mechtron mechtrondev@gmail.com <corey@tophat.com>
2016-06-01 16:53:52 -0400

    Add support for rolling stack updates

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
   --stack-name "town-clock" \
   --capabilities CAPABILITY_IAM \
   --template-body file://./unreliable-town-clock.template \
   --parameters \
-  ParameterKey='ClockPeriod',ParameterValue='10',UsePreviousValue=false \
+  ParameterKey='ClockPeriod',ParameterValue='60',UsePreviousValue=false \
   ParameterKey='InstallScriptURL',ParameterValue='https://raw.githubusercontent.com/tophatmonocle/alestic-unreliable-town-clock/master/unreliable-town-clock-bootstrap',UsePreviousValue=false \
   ParameterKey='InstanceType',ParameterValue='t2.micro',UsePreviousValue=false \
   ParameterKey='OperatorEmail',ParameterValue='corey@tophat.com',UsePreviousValue=false \
